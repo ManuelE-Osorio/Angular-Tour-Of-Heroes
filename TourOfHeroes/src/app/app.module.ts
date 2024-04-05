8f7cb93dee00221edc42bc50fc197a518c2b625d
ManuelE-Osorio <aux.manuel.osorio@gmail.com>
2024-04-05 02:34:47 -0500

    Tutorial till P3

---
+++ TourOfHeroes/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { NgFor } from '@angular/common';
+import { NgIf } from '@angular/common';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    NgFor,
+    NgIf
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ TourOfHeroes/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,11 @@
+import {Hero} from '../hero';
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrl: './hero-detail.component.css'
+})
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
+}
